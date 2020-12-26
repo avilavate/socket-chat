73ccba9981d2a24242a9a787a29b368b89d2e088
Avil Avate <avilavate123@gmail.com>
2020-12-26 12:10:57 +0530

    Fixed client id feature

--- server/server.js
+++ server/server.js
@@ -1,11 +1,20 @@
 const netSock= require('net');
 
 server = netSock.createServer();
+let counter=0;
+let sockets=[];
 
 server.on('connection',(socket)=>{
-    console.log('server connected');
+    socket.id=counter++;
+    sockets.push({'id':socket.id,'socket':socket});
+    socket.write(`Welcome client id: ${socket.id}`)
+    console.log(`server connected ${socket.id}`);
     socket.on('data',(data)=>{
-        console.log(data);
+    sockets.forEach(s=>{
+        
+        s.socket.write(` ${socket.id} `);
+        s.socket.write(`${data}`);
+    })
     });
     socket.setEncoding('UTF8');
    });
